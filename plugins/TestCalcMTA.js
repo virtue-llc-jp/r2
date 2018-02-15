9527b3072c92f4bfb4de30652d388d2fb438d171
PeterGrainfield <peter@vollegunord.work>
2018-02-16 06:06:36 +0900

    add test plugin

---
+++ plugins/TestCalcMTA.js
@@ -0,0 +1,61 @@
+const _ = require('lodash');
+const ss = require('simple-statistics');
+const { getLogger } = require('@bitr/logger');
+
+const precision = 3;
+const sigma_power = 2.0; // 標準偏差の倍率
+const profit_ratio = 0.75; // 偏差のうちNetProfitとする割合
+
+class TestCalcMTA {
+  // Constructor is called when initial snapshot of spread stat history has arrived.
+  constructor(history) {
+    this.history = history; //historyを保存
+    this.log = getLogger(this.constructor.name);
+    this.profitPercentHistory = this.history.map(x => x.bestCase.profitPercentAgainstNotional);
+    this.sampleSize = this.profitPercentHistory.length;
+    this.profitPercentMean = this.sampleSize != 0 ? ss.mean(this.profitPercentHistory) : 0;
+    this.profitPercentVariance = this.sampleSize != 0 ? ss.variance(this.profitPercentHistory) : 0;
+  }
+
+  // The method is called each time new spread stat has arrived, by default every 3 seconds.
+  // Return value: part of ConfigRoot or undefined.
+  // If part of ConfigRoot is returned, the configuration will be merged. If undefined is returned, no update will be made.
+  async handle(spreadStat) {
+    this.history = _.tail(this.history);
+    this.history.push(spreadStat);
+    this.profitPercentHistory = this.history.map(x => x.bestCase.profitPercentAgainstNotional);
+    this.profitPercentMean = this.sampleSize != 0 ? ss.mean(this.profitPercentHistory) : 0;
+    this.profitPercentVariance = this.sampleSize != 0 ? ss.variance(this.profitPercentHistory) : 0;
+
+    // set μ + σ to minTargetProfitPercent
+    const n = this.sampleSize;
+    const mean = this.profitPercentMean;
+    const standardDeviation = Math.sqrt(this.profitPercentVariance * n/(n-1));
+    const minTargetProfitPercent = _.round(mean + (standardDeviation * sigma_power), precision);
+
+    // cost
+    
+
+    // exitNetProfitRation by standardDeviation 
+    const exitNetProfitRatio = _.round(
+      standardDeviation * sigma_power * profit_ratio / minTargetProfitPercent, precision
+    );
+
+    // error
+    if (_.isNaN(minTargetProfitPercent) || _.isNaN(exitNetProfitRatio)) {
+      return undefined;
+    }
+    this.log.info(
+      `μ: ${_.round(mean, precision)}, σ: ${_.round(
+        standardDeviation,
+        precision
+      )}, n: ${n} => minTargetProfitPercent: ${minTargetProfitPercent}`
+    );
+
+    // save config
+    const config = { minTargetProfitPercent, exitNetProfitRatio };
+    return config;
+  }
+}
+
+module.exports = TestCalcMTA;
