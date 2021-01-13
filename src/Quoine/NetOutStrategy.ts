89a2864ab93c1bbed2041974700c2a801980c689
kikuchi-shunsuke-virtue-llc-jp <kikuchi.shunsuke@virtue.llc>
2021-01-13 11:04:59 +0900

    Fix error message.

--- src/Quoine/NetOutStrategy.ts
+++ src/Quoine/NetOutStrategy.ts
@@ -9,7 +9,7 @@ export default class NetOutStrategy implements CashMarginTypeStrategy {
     const accounts = await this.brokerApi.getTradingAccounts();
     const account = _.find(accounts, b => b.currency_pair_code === 'BTCJPY');
     if (!account) {
-      throw new Error('Unable to find the account.');
+      throw new Error('Quoine - NetOutStrategy: Unable to find the account.');
     }
     return account.position;
   }
